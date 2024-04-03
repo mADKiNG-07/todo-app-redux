0e29386195063f5bedade1f1bdf8b480750bcb45
mADKiNG-07 <jordanmhizcleo@gmail.com>
2024-04-03 11:06:36 +0100

    bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ backgroundColor: "black" }}>
       <body className={inter.className}>{children}</body>
     </html>
   );
